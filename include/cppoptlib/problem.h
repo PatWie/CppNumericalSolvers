d94f3e9a86f6a3ef43486c83a8d6b23dfa855e1a
Фёдоров Николай <unril@live.ru>
2016-06-10 17:02:58 +0300

    Update problem.h
    Reformat to pass lint. Replace ptrdiff_t to Eigen::Index.

--- include/cppoptlib/problem.h
+++ include/cppoptlib/problem.h
@@ -16,6 +16,7 @@ class Problem {
   using TVector   = Eigen::Matrix<Scalar, Dim, 1>;
   using THessian  = Eigen::Matrix<Scalar, Dim, Dim>;
   using TCriteria = Criteria<Scalar>;
+  using TIndex = typename TVector::Index;
 
  public:
   Problem() {}
@@ -59,18 +60,17 @@ class Problem {
    */
   virtual void hessian(const TVector &x, THessian &hessian) {
     finiteHessian(x, hessian);
-
   }
 
   virtual bool checkGradient(const TVector &x, int accuracy = 3) {
     // TODO: check if derived class exists:
     // int(typeid(&Rosenbrock<double>::gradient) == typeid(&Problem<double>::gradient)) == 1 --> overwritten
-    const ptrdiff_t D = x.rows();
+    const TIndex D = x.rows();
     TVector actual_grad(D);
     TVector expected_grad(D);
     gradient(x, actual_grad);
     finiteGradient(x, expected_grad, accuracy);
-    for (ptrdiff_t d = 0; d < D; ++d) {
+    for (TIndex d = 0; d < D; ++d) {
       Scalar scale = std::max((std::max(fabs(actual_grad[d]), fabs(expected_grad[d]))), 1.);
       if(fabs(actual_grad[d]-expected_grad[d])>1e-2 * scale)
         return false;
@@ -82,14 +82,14 @@ class Problem {
   virtual bool checkHessian(const TVector &x, int accuracy = 3) {
     // TODO: check if derived class exists:
     // int(typeid(&Rosenbrock<double>::gradient) == typeid(&Problem<double>::gradient)) == 1 --> overwritten
-    const ptrdiff_t D = x.rows();
+    const TIndex D = x.rows();
 
     THessian actual_hessian = THessian::Zero(D, D);
     THessian expected_hessian = THessian::Zero(D, D);
     hessian(x, actual_hessian);
     finiteHessian(x, expected_hessian, accuracy);
-    for (ptrdiff_t d = 0; d < D; ++d) {
-      for (ptrdiff_t e = 0; e < D; ++e) {
+    for (TIndex d = 0; d < D; ++d) {
+      for (TIndex e = 0; e < D; ++e) {
         Scalar scale = std::max(static_cast<Scalar>(std::max(fabs(actual_hessian(d, e)), fabs(expected_hessian(d, e)))), Scalar(1.));
         if(fabs(actual_hessian(d, e)- expected_hessian(d, e))>1e-1 * scale)
           return false;
@@ -98,46 +98,47 @@ class Problem {
     return true;
   }
 
-  virtual void finiteGradient(const  TVector &x, TVector &grad, int accuracy = 0) final {
+  void finiteGradient(const  TVector &x, TVector &grad, int accuracy = 0) {
     // accuracy can be 0, 1, 2, 3
     const Scalar eps = 2.2204e-6;
     const Scalar coeff[4][8] = {
-	  {1, -1, 0, 0, 0, 0, 0, 0}, 
-	  {1, -8, 8, -1, 0, 0, 0, 0},
-	  {-1, 9, -45, 45, -9, 1, 0, 0},
-	  {3, -32, 168, -672, 672, -168, 32, -3}
-	};
-    const Scalar coeff2[4][8] = { 
-      {1, -1, 0, 0, 0, 0, 0, 0}, 
-	  {-2, -1, 1, 2, 0, 0, 0, 0}, 
-	  {-3, -2, -1, 1, 2, 3, 0, 0},
-	  {-4, -3, -2, -1, 1, 2, 3, 4} 
-	};
+      {1, -1, 0, 0, 0, 0, 0, 0},
+      {1, -8, 8, -1, 0, 0, 0, 0},
+      {-1, 9, -45, 45, -9, 1, 0, 0},
+      {3, -32, 168, -672, 672, -168, 32, -3}
+    };
+    const Scalar coeff2[4][8] = {
+      {1, -1, 0, 0, 0, 0, 0, 0},
+      {-2, -1, 1, 2, 0, 0, 0, 0},
+      {-3, -2, -1, 1, 2, 3, 0, 0},
+      {-4, -3, -2, -1, 1, 2, 3, 4}
+    };
     const Scalar dd[4] = {2, 12, 60, 840};
 
-    TVector finiteDiff(x.rows());
+    grad.resize(x.rows());
     TVector xx(x.rows());
-	const int innerSteps = 2*(accuracy+1);
-	for (ptrdiff_t d = 0; d < x.rows(); d++) {
-      finiteDiff[d] = 0;
+    const int innerSteps = 2*(accuracy+1);
+
+    for (TIndex d = 0; d < x.rows(); d++) {
+      grad[d] = 0;
       for (int s = 0; s < innerSteps; ++s)
       {
         xx = x;
         xx[d] += coeff2[accuracy][s]*eps;
-        finiteDiff[d] += coeff[accuracy][s]*value(xx);
+        grad[d] += coeff[accuracy][s]*value(xx);
       }
-      finiteDiff[d] /= (dd[accuracy]* eps);
+      grad[d] /= (dd[accuracy]* eps);
     }
-    grad = finiteDiff;
   }
 
-  virtual void finiteHessian(const TVector &x, THessian &hessian, int accuracy = 0) final {
+  void finiteHessian(const TVector &x, THessian &hessian, int accuracy = 0) {
     const Scalar eps = std::numeric_limits<Scalar>::epsilon()*10e7;
 
-	TVector xx(x.rows());
+    hessian.resize(x.rows(), x.rows());
+    TVector xx(x.rows());
     if(accuracy == 0) {
-      for (ptrdiff_t i = 0; i < x.rows(); i++) {
-        for (ptrdiff_t j = 0; j < x.rows(); j++) {
+      for (TIndex i = 0; i < x.rows(); i++) {
+        for (TIndex j = 0; j < x.rows(); j++) {
           xx = x;
           Scalar f4 = value(xx);
           xx[i] += eps;
@@ -161,8 +162,8 @@ class Problem {
           74(f_{-1,-1}+f_{1,1}-f_{1,-1}-f_{-1,1})
         \end{matrix}\right] }
       */
-      for (ptrdiff_t i = 0; i < x.rows(); i++) {
-        for (ptrdiff_t j = 0; j < x.rows(); j++) {
+      for (TIndex i = 0; i < x.rows(); i++) {
+        for (TIndex j = 0; j < x.rows(); j++) {
 
           Scalar term_1 = 0;
           xx = x.eval(); xx[i] += 1*eps;  xx[j] += -2*eps;  term_1 += value(xx);
